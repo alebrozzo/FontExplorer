0b07daf96cb2c69885a4b47b1a80d8132021bfcd
Alejandro Brozzo <alebrozzo@msn.com>
2021-02-15 19:54:31 +1300

    Don't open form if already opened

--- FontExplorer/mdiContainer.cs
+++ FontExplorer/mdiContainer.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System.Linq;
+using System.Windows.Forms;
 
 namespace FontExplorer
 {
@@ -11,6 +12,12 @@ namespace FontExplorer
 
     private void mnsMainMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
     {
+      bool formAlreadyOpen = this.MdiChildren.Any(form => form.Name == e.ClickedItem.Tag.ToString());
+      if (formAlreadyOpen)
+      {
+        return;
+      }
+
       Form childForm = e.ClickedItem.Tag switch
       {
         "frmExplorer" => new frmExplorer(),
